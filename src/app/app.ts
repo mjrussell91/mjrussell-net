fce585d53332d19dd450665aebbc77cc4f0c7674
Matthew Russell <mjrussell91@gmail.com>
2025-07-31 16:35:40 +1000

    add header and footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: 'app-root',
+	imports: [RouterOutlet, Header, Footer],
+	templateUrl: './app.html',
+	styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Matthew Russell');
+	protected readonly title = signal('Matthew Russell');
 }

d480a9ef9f4334dc6ee1cfd65265a1f7bdbca2b4
Matthew Russell <mjrussell91@gmail.com>
2025-08-01 21:27:17 +1000

    basic header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 	styleUrl: './header.css'
 })
 export class Header {
-	
+	protected readonly email: string = 'contact@test.com';
+	// Obfuscation: https://spencermortensen.com/articles/email-obfuscation/
+	// https://stackoverflow.com/questions/41318987/hide-email-address-from-bots-keep-mailto
+	protected readonly phone: string = '+614 1234 5678';
+	protected readonly github: string = 'https://github.com/mjrussell91';
+	protected readonly website: string = 'https://mjrussell.net';
 }

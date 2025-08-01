18744f0024fe99c80f0a9241270aee28ea9afa6c
Matthew Russell <mjrussell91@gmail.com>
2025-08-01 21:27:05 +1000

    icon component

--- src/app/icon/icon.ts
+++ src/app/icon/icon.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 	styleUrl: './icon.css'
 })
 export class Icon {
+	// Docs: https://v17.angular.io/guide/svg-in-templates
+	// https://stackoverflow.com/questions/46101539/is-there-a-way-to-add-source-to-an-svg-element
+
+
+	// Icons: https://www.svgrepo.com/svg/437851/email
 	// public fillColor = 'var(--color-white-1)';
 	public fillColor = 'red';
 }

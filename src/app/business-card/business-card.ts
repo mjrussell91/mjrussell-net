74264ae900eaddf8935d50a4707c4ec2c2228f70
Matthew Russell <mjrussell91@gmail.com>
2025-08-01 21:26:19 +1000

    business card component

--- src/app/business-card/business-card.ts
+++ src/app/business-card/business-card.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Icon } from '../icon/icon';
 
 @Component({
-  selector: 'app-business-card',
-  imports: [],
-  templateUrl: './business-card.html',
-  styleUrl: './business-card.css'
+	selector: 'app-business-card',
+	imports: [Icon],
+	templateUrl: './business-card.html',
+	styleUrl: './business-card.css'
 })
 export class BusinessCard {
-
+	protected readonly email: string = 'contact@test.com';
+	protected readonly phone: string = '+614 1234 5678';
+	protected readonly github: string = 'https://github.com/mjrussell91';
+	protected readonly website: string = 'https://mjrussell.net';
 }

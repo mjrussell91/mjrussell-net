dcc048717355b5fe3b26b67fbe2db565c922029b
Matthew Russell <mjrussell91@gmail.com>
2025-08-01 21:17:43 +1000

    icon component

---
+++ src/app/icon/icon.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-icon',
+	imports: [],
+	templateUrl: './icon.html',
+	styleUrl: './icon.css'
+})
+export class Icon {
+	// public fillColor = 'var(--color-white-1)';
+	public fillColor = 'red';
+}

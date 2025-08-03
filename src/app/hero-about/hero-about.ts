6e4e2bfcfdd7d01850ba3144647630a68c3306be
Matthew Russell <mjrussell91@gmail.com>
2025-08-03 17:59:55 +1000

    hero about component

---
+++ src/app/hero-about/hero-about.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-about',
+  imports: [],
+  templateUrl: './hero-about.html',
+  styleUrl: './hero-about.css'
+})
+export class HeroAbout {
+
+}

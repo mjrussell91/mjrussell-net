e79761bd363424670fe18f8c6fa2f7d9ad9f6bdd
Matthew Russell <mjrussell91@gmail.com>
2025-08-03 12:08:56 +1000

    dyanmic nav links for nav array

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-header',
-	imports: [],
+	imports: [CommonModule],
 	templateUrl: './header.html',
 	styleUrl: './header.css'
 })
 export class Header {
-
+	public navs = [
+		{ label: 'About', route: 'about' },
+		{ label: 'CV', route: 'CV' },
+		{ label: 'Projects', route: 'projects' },
+		{ label: 'Contact', route: 'contact' }
+	]
 }

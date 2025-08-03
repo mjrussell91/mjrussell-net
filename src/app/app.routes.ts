5ac8a5ca3eae8815950a07a5759b1c860253bfe9
Matthew Russell <mjrussell91@gmail.com>
2025-08-03 11:51:12 +1000

    header nav fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BusinessCard } from './business-card/business-card';
+import { PageNotFound } from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'about', component: BusinessCard},
+    { path: '**', component: PageNotFound}, // 404 / Page Not Found
+];

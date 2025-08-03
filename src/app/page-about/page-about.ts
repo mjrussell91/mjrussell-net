d98b73b3588ec38541a40320d7937cd4ea88f462
Matthew Russell <mjrussell91@gmail.com>
2025-08-03 21:06:30 +1000

    about page update

--- src/app/page-about/page-about.ts
+++ src/app/page-about/page-about.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-page-about',
-  imports: [],
-  templateUrl: './page-about.html',
-  styleUrl: './page-about.css'
+	selector: 'app-page-about',
+	imports: [],
+	templateUrl: './page-about.html',
+	styleUrl: './page-about.css'
 })
 export class PageAbout {
 

5fc55af1d72d5aaa3c842b1af2d5ee3757693cac
Matthew Russell <mjrussell91@gmail.com>
2025-08-06 09:46:32 +1000

    about page content and formatting

--- src/app/page-about/page-about.ts
+++ src/app/page-about/page-about.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
 	selector: 'app-page-about',
-	imports: [],
+	imports: [CommonModule, RouterModule],
 	templateUrl: './page-about.html',
 	styleUrl: './page-about.css'
 })
 export class PageAbout {
-
+	protected readonly profileSrc = 'profile.jpg';
 }

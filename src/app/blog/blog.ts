18cc4152309a4b72fd1ac2e633c24e5010cf7330
Matthew Russell <mjrussell91@gmail.com>
2025-09-22 23:47:28 +1000

    add blog post type and array, basic blog list

--- src/app/blog/blog.ts
+++ src/app/blog/blog.ts
@@ -1,9 +1,26 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { DatePipe } from "@angular/common"
 
+interface BlogPost {
+	title: string;
+	slug: string;
+	date: Date;
+	excerpt: string;
+};
 @Component({
 	selector: "app-blog",
-	imports: [],
+	imports: [RouterLink, DatePipe],
 	templateUrl: "./blog.html",
 	styleUrl: "./blog.css",
 })
-export class Blog {}
+export class Blog {
+	public readonly blogs: BlogPost[] = [
+		{
+			title: "Bioprospecting Honeycomb",
+			slug: "bioprospecting-honeycomb",
+			date: new Date("2025-10-07"),
+			excerpt: "Finding unique local yeasts and bacteria in honeycomb.",
+		},
+	];
+}
